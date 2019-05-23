f0f8c822278d14063c03045b4fbbd24a2be412a0
Ninad Sachania <ninad.sachania@gmail.com>
2019-05-23 18:45:14 +0530

    Init commit

---
+++ basic.sh
@@ -0,0 +1,25 @@
+sudo apt -y install \
+vim \
+python3 \
+git \
+gcc \
+g++ \
+make \
+clang \
+pandoc \
+ranger \
+sxiv \
+vlc \
+tmux \
+figlet \
+calcurse \
+mpd \
+mpc \
+ncmpcpp \
+imagemagick \
+xclip \
+zathura \
+zathura-djvu \
+texlive \
+cowsay \
+lolcat \
